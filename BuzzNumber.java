58922fed9af09d6e6a8a512607e8fe4865797dff
RiOxFRANKY <abhirupsar2005@gmail.com>
2025-07-01 23:05:59 +0530

    Update Buzz Number

--- BuzzNumber.java
+++ BuzzNumber.java
@@ -1,37 +1,14 @@
-package com.company;
-import java.util.Scanner;
+import java.util.*;
+
 public class BuzzNumber {
 
     public static void main(String[] args) {
-        boolean isPositive = false;
-        // while loop and isPositive flag will assure integer entered is positive
-        while (isPositive == false) {
-            int checkBuzz = -1;
-            System.out.println("Input an integer: ");
-            boolean validEntry = false;
-            // while loop with try catch to make sure input is valid integer
-            while (validEntry == false) {
-                try {
-                    Scanner in = new Scanner(System.in);
-                    checkBuzz = in.nextInt();
-                    validEntry = true;
-                } catch (Exception e) {
-                    System.out.println("You didn't enter an integer. Re-enter...");
-                }
-            }
-            // if statement to continue loop if input was a negative number.
-            if (checkBuzz < 0) {
-                continue;
-            }
-            boolean isBuzzNumber = false;
-            if (checkBuzz % 7 == 0 || checkBuzz % 10 == 7){ // Checks if input can be divisible by 7 or ends with 7
-                isBuzzNumber = true;
-            }
-            isPositive = true;
-            System.out.println("isBuzzNumber: " + isBuzzNumber);
-
+        Scanner sc = sc.nextInt();
+        String s = sc.next();
+        if (s.charAt(s.length()-1) == '7' || Long.parseLong(s)%7==0) {
+            System.out.println(s+" is a Buzz Number");
         }
-        
-        
+        else System.out.println(s+" is not a Buzz Number");
+        sc.close();
     }
 }
